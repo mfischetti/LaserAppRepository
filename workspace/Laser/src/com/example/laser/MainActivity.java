375055b14cb7d1d8f7ef38b09fed4699c39e4753
mjod <mjod@udel.edu>
2013-10-01 21:07:49 -0400

    Laser app commit
    hi mike

---
+++ workspace/Laser/src/com/example/laser/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.laser;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	TextView mTitle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setupWidget();
+    }
+
+
+    private void setupWidget() {
+    	Button host = (Button)findViewById(R.id.host);
+    	host.setOnClickListener(this);
+    	Button find = (Button)findViewById(R.id.find);
+    	find.setOnClickListener(this);
+    	Button help = (Button)findViewById(R.id.help);
+    	help.setOnClickListener(this);
+    	mTitle = (TextView)findViewById(R.id.title);
+    	mTitle.setTextColor(Color.BLUE);
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		if(v.getId() == R.id.host){
+			StartHost();
+		}
+		if(v.getId() == R.id.find){
+			StartFind();
+		}
+		if(v.getId() == R.id.help){
+			StartHelp();
+		}
+	}
+
+
+	private void StartHelp() {
+		Intent shelp = new Intent(this, HelpActivity.class);	
+		startActivity(shelp);		
+	}
+
+
+	private void StartFind() {
+		Intent sfind = new Intent(this, FindActivity.class);	
+		startActivity(sfind);		
+	}
+
+
+	private void StartHost() {
+		Intent shost = new Intent(this, HostActivity.class);	
+		startActivity(shost);
+	}
+    
+}
