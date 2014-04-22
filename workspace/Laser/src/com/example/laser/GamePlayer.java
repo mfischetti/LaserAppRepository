e84a55cfcf43680f388f90e233f66c5445292224
mjod <mjod@udel.edu>
2014-04-21 20:58:40 -0400

    gameplayer

---
+++ workspace/Laser/src/com/example/laser/GamePlayer.java
@@ -0,0 +1,37 @@
+package com.example.laser;
+
+public class GamePlayer {
+	String Player;
+	String Player1;
+	String Player2;
+	String Player3;
+	String Player4;
+	String Player5;
+	String Player6;
+	String Player7;
+	String Player8;
+	String Team;
+	String xLoc, yLoc;
+	String Score;
+	String NameID;
+
+	public GamePlayer(String Player, String Player1, String Player2, String Player3,
+						String Player4, String Player5, String Player6, String Player7, String Player8,
+						String Team, String xLoc, String yLoc, String Score, String NameID){
+		this.Player = Player;
+		this.Player1 = Player1;
+		this.Player2 = Player2;
+		this.Player3 = Player3;
+		this.Player4 = Player4;
+		this.Player5 = Player5;
+		this.Player6 = Player6;
+		this.Player7 = Player7;
+		this.Player8 = Player8;
+		this.Team = Team;
+		this.xLoc = xLoc;
+		this.yLoc = yLoc;
+		this.Score = Score;
+		this.NameID = NameID;
+	}
+
+}
